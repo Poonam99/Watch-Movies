89c34027c3d9aa2584322b921e486430d58416aa
Poonam99 <poonamtabassum9909@gmail.com>
2022-09-29 22:47:08 +0600

    first commit

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            <nav className="navbar bg-light">
+                <div className="container-fluid">
+
+                    <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top " />
+                    Bootstrap
+
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Nav;
